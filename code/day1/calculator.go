4ba1dd27f03036a414f7e81472af0355bdc2dba1
Giovanni Guzman Cespedes <giovanni.guzman@AvalDigitalLabs.com>
2021-02-21 18:14:58 -0500

    Se completa el dia 1 de la kata

---
+++ code/day1/calculator.go
@@ -0,0 +1,16 @@
+package calculator
+
+import "strconv"
+
+func Add(numbers string) int {
+	if len(numbers) == 0 {
+		return 0
+	} else if len(numbers) == 1 {
+		number, _ := strconv.Atoi(numbers)
+		return number
+	} else {
+		firstNumber, _ := strconv.Atoi(numbers[:1])
+		secondNumber, _ := strconv.Atoi(numbers[2:])
+		return firstNumber + secondNumber
+	}
+}
